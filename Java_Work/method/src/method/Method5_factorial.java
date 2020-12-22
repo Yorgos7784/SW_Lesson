2d86a508b5a7155ff0930b3483488d9a8b0ccfc9
Yorgos7784 <ghkd7784@gmail.com>
2020-12-22 13:39:29 +0900

    20201222 java 수업내용

--- Java_Work/method/src/method/Method5_factorial.java
+++ Java_Work/method/src/method/Method5_factorial.java
@@ -7,13 +7,14 @@ public class Method5_factorial {
 		System.out.println("3 factorial : " + factorial(3));
 		System.out.println("12 factorial : " + factorial(12));
 	}
-	
+
 	public static int factorial(int n) {
-		if(n == 1) {
+		// 5 factorial = 5 * 4 * 3 * 2 * 1
+		// = 5 * 4 factorial
+		if (n == 1) 
 			return 1;
-		}
 		else
-			return n * factorial(n-1);
+			return n * factorial(n - 1);
 	}
 
 }
