2ae16d96d3c8e6342a459da2f6386e426ca4fd47
Yorgos7784 <ghkd7784@gmail.com>
2020-12-30 12:13:50 +0900

    20201230 Java

--- Java_Work/array_20201223/src/array_20201223/Array_practice7.java
+++ Java_Work/array_20201223/src/array_20201223/Array_practice7.java
@@ -13,13 +13,13 @@ import java.util.StringTokenizer;
 
 public class Array_practice7 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		Scanner s = new Scanner(System.in);
 		Random rand = new Random();
-		BufferedOutputStream bs;
+		BufferedOutputStream bs = null;
 		BufferedReader br;
 		
-		String last[] = {"김", "박", "이", "최", "장"};
+		/*String last[] = {"김", "박", "이", "최", "장"};
 		String first[] = {"바", "사", "아", "자", "차"};
 		String mid[] = {"가", "나", "다", "라", "마"};
 		String fullname[] = new String[50];
@@ -94,9 +94,55 @@ public class Array_practice7 {
 				continue;
 			}
 	
+		}*/
+		final String last[] = {"김", "박", "이", "최", "장"};
+		final String first[] = {"바", "사", "아", "자", "차"};
+		final String mid[] = {"가", "나", "다", "라", "마"};
+		String fullname[] = new String[50];
+		
+		while(true) {
+			System.out.println("-----------------------");
+			System.out.println("  파일 제어 프로그램 v1.1");
+			System.out.println("-----------------------");
+			System.out.println("1. 이름 생성하여 파일에 저장");
+			System.out.println("2. 파일에서 이름 읽어오기");
+			System.out.println("3. 프로그램 종료");
+			System.out.println("-----------------------");
+			System.out.print("메뉴 선택 : ");
+			int menu = s.nextInt();
+			System.out.println("-----------------------");
+			switch(menu) {
+			case 1:
+				System.out.println("1. 이름 생성하여 파일에 저장");
+				bs = new BufferedOutputStream(
+						new FileOutputStream("d:/names.txt"));
+				for (int i = 0; i < fullname.length; i++) {
+					fullname[i] = last[(int)(Math.random()*5)] + first[(int)(Math.random()*5)] + mid[(int)(Math.random()*5)];
+					bs.write(fullname[i].getBytes());
+					bs.write("/".getBytes());
+				}
+				bs.close();
+				System.out.println("파일 저장 완료!");
+				break;
+			case 2:
+				br = new BufferedReader(new FileReader(new File("d:/names.txt")));
+				String str = br.readLine();
+				StringTokenizer stk = new StringTokenizer(str,"/");
+				while(stk.hasMoreTokens()) {
+					System.out.println(stk.nextToken());}
+				br.close();
+				System.out.println("읽어오기 완료!");
+				break;
+			case 3:
+				// 프로세스 : 실행중인 프로그램
+				// 프로세서 : CPU
+				s.close();
+				System.exit(0);
+			default:
+				System.out.println("잘못된 메뉴 선택!");
+				break;
+			}
 		}
-				
-
 	}
 
 }
