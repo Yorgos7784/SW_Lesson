d00a60a136917c84f245327438c076eb1a5f6792
Yorgos7784 <ghkd7784@gmail.com>
2021-01-07 09:36:24 +0900

    20210107

---
+++ java/baekjoon/break_even_point/src/break_even_point/Main.java
@@ -0,0 +1,20 @@
+package break_even_point;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		long a = s.nextInt();
+		long b = s.nextInt();
+		long c = s.nextInt();
+		int count = 1;
+		if(c <= b) {
+			System.out.println(-1);
+		}
+		else {
+			
+		}
+	}
+}
