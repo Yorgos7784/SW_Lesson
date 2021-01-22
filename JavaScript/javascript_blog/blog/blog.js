715a760ddf1d129029bb9331868a62514ad400f8
Yorgos7784 <ghkd7784@gmail.com>
2021-01-22 16:43:32 +0900

    20210122 javascript

--- JavaScript/javascript_blog/blog/blog.js
+++ JavaScript/javascript_blog/blog/blog.js
@@ -12,4 +12,41 @@ $(document).ready(function () {
             $(activeTab).fadeIn();
             return false;
         });
+
+});
+
+const showoverlay = document.querySelector(".movie_blackbird");
+const overlay = document.querySelector("#blackbird");
+showoverlay.addEventListener('click', function () {
+    overlay.classList.add("show");
+});
+overlay.addEventListener('click', function () {
+    overlay.classList.remove("show");
+});
+
+const showoverlay2 = document.querySelector(".movie_nightingale");
+const overlay2 = document.querySelector("#nightingale");
+showoverlay2.addEventListener('click', function () {
+    overlay2.classList.add("show");
+});
+overlay2.addEventListener('click', function () {
+    overlay2.classList.remove("show");
+});
+
+const showoverlay3 = document.querySelector("#side_lobster");
+const overlay3 = document.querySelector("#lobster");
+showoverlay3.addEventListener('click', function () {
+    overlay3.classList.add("show");
+});
+overlay3.addEventListener('click', function () {
+    overlay3.classList.remove("show");
+});
+
+const showoverlay4 = document.querySelector("#side_arrival");
+const overlay4 = document.querySelector("#arrival");
+showoverlay4.addEventListener('click', function () {
+    overlay4.classList.add("show");
+});
+overlay4.addEventListener('click', function () {
+    overlay4.classList.remove("show");
 });
