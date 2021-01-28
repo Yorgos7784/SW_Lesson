7fb1e70aa9fd2e74c4a47a8652b0583d1c4b3246
Yorgos7784 <ghkd7784@gmail.com>
2021-01-28 16:30:37 +0900

    20210128 C#

---
+++ C_Shop_Lesson/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,284 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Numpad1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Numpad2 = new System.Windows.Forms.Button();
+            this.Numpad3 = new System.Windows.Forms.Button();
+            this.Numpad4 = new System.Windows.Forms.Button();
+            this.Numpad5 = new System.Windows.Forms.Button();
+            this.Numpad6 = new System.Windows.Forms.Button();
+            this.Numpad7 = new System.Windows.Forms.Button();
+            this.Numpad8 = new System.Windows.Forms.Button();
+            this.Numpad9 = new System.Windows.Forms.Button();
+            this.Numpadp = new System.Windows.Forms.Button();
+            this.Numpadm = new System.Windows.Forms.Button();
+            this.Numpadd = new System.Windows.Forms.Button();
+            this.Numpadg = new System.Windows.Forms.Button();
+            this.Numpad0 = new System.Windows.Forms.Button();
+            this.result = new System.Windows.Forms.Button();
+            this.Clear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Numpad1
+            // 
+            this.Numpad1.Font = new System.Drawing.Font("굴림", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Numpad1.Location = new System.Drawing.Point(21, 56);
+            this.Numpad1.Name = "Numpad1";
+            this.Numpad1.Size = new System.Drawing.Size(65, 65);
+            this.Numpad1.TabIndex = 0;
+            this.Numpad1.Text = "1";
+            this.Numpad1.UseVisualStyleBackColor = true;
+            this.Numpad1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(75, 24);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(174, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Numpad2
+            // 
+            this.Numpad2.Font = new System.Drawing.Font("굴림", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Numpad2.Location = new System.Drawing.Point(92, 56);
+            this.Numpad2.Name = "Numpad2";
+            this.Numpad2.Size = new System.Drawing.Size(65, 65);
+            this.Numpad2.TabIndex = 0;
+            this.Numpad2.Text = "2";
+            this.Numpad2.UseVisualStyleBackColor = true;
+            this.Numpad2.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Numpad3
+            // 
+            this.Numpad3.Font = new System.Drawing.Font("굴림", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Numpad3.Location = new System.Drawing.Point(163, 56);
+            this.Numpad3.Name = "Numpad3";
+            this.Numpad3.Size = new System.Drawing.Size(65, 65);
+            this.Numpad3.TabIndex = 0;
+            this.Numpad3.Text = "3";
+            this.Numpad3.UseVisualStyleBackColor = true;
+            this.Numpad3.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Numpad4
+            // 
+            this.Numpad4.Font = new System.Drawing.Font("굴림", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Numpad4.Location = new System.Drawing.Point(21, 127);
+            this.Numpad4.Name = "Numpad4";
+            this.Numpad4.Size = new System.Drawing.Size(65, 65);
+            this.Numpad4.TabIndex = 0;
+            this.Numpad4.Text = "4";
+            this.Numpad4.UseVisualStyleBackColor = true;
+            this.Numpad4.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Numpad5
+            // 
+            this.Numpad5.Font = new System.Drawing.Font("굴림", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Numpad5.Location = new System.Drawing.Point(92, 127);
+            this.Numpad5.Name = "Numpad5";
+            this.Numpad5.Size = new System.Drawing.Size(65, 65);
+            this.Numpad5.TabIndex = 0;
+            this.Numpad5.Text = "5";
+            this.Numpad5.UseVisualStyleBackColor = true;
+            this.Numpad5.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Numpad6
+            // 
+            this.Numpad6.Font = new System.Drawing.Font("굴림", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Numpad6.Location = new System.Drawing.Point(163, 127);
+            this.Numpad6.Name = "Numpad6";
+            this.Numpad6.Size = new System.Drawing.Size(65, 65);
+            this.Numpad6.TabIndex = 0;
+            this.Numpad6.Text = "6";
+            this.Numpad6.UseVisualStyleBackColor = true;
+            this.Numpad6.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Numpad7
+            // 
+            this.Numpad7.Font = new System.Drawing.Font("굴림", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Numpad7.Location = new System.Drawing.Point(21, 198);
+            this.Numpad7.Name = "Numpad7";
+            this.Numpad7.Size = new System.Drawing.Size(65, 65);
+            this.Numpad7.TabIndex = 0;
+            this.Numpad7.Text = "7";
+            this.Numpad7.UseVisualStyleBackColor = true;
+            this.Numpad7.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Numpad8
+            // 
+            this.Numpad8.Font = new System.Drawing.Font("굴림", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Numpad8.Location = new System.Drawing.Point(92, 198);
+            this.Numpad8.Name = "Numpad8";
+            this.Numpad8.Size = new System.Drawing.Size(65, 65);
+            this.Numpad8.TabIndex = 0;
+            this.Numpad8.Text = "8";
+            this.Numpad8.UseVisualStyleBackColor = true;
+            this.Numpad8.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Numpad9
+            // 
+            this.Numpad9.Font = new System.Drawing.Font("굴림", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Numpad9.Location = new System.Drawing.Point(163, 198);
+            this.Numpad9.Name = "Numpad9";
+            this.Numpad9.Size = new System.Drawing.Size(65, 65);
+            this.Numpad9.TabIndex = 0;
+            this.Numpad9.Text = "9";
+            this.Numpad9.UseVisualStyleBackColor = true;
+            this.Numpad9.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Numpadp
+            // 
+            this.Numpadp.Font = new System.Drawing.Font("굴림", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Numpadp.Location = new System.Drawing.Point(234, 56);
+            this.Numpadp.Name = "Numpadp";
+            this.Numpadp.Size = new System.Drawing.Size(65, 65);
+            this.Numpadp.TabIndex = 0;
+            this.Numpadp.Text = "+";
+            this.Numpadp.UseVisualStyleBackColor = true;
+            this.Numpadp.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Numpadm
+            // 
+            this.Numpadm.Font = new System.Drawing.Font("굴림", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Numpadm.Location = new System.Drawing.Point(234, 127);
+            this.Numpadm.Name = "Numpadm";
+            this.Numpadm.Size = new System.Drawing.Size(65, 65);
+            this.Numpadm.TabIndex = 0;
+            this.Numpadm.Text = "-";
+            this.Numpadm.UseVisualStyleBackColor = true;
+            this.Numpadm.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Numpadd
+            // 
+            this.Numpadd.Font = new System.Drawing.Font("굴림", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Numpadd.Location = new System.Drawing.Point(234, 269);
+            this.Numpadd.Name = "Numpadd";
+            this.Numpadd.Size = new System.Drawing.Size(65, 65);
+            this.Numpadd.TabIndex = 0;
+            this.Numpadd.Text = "/";
+            this.Numpadd.UseVisualStyleBackColor = true;
+            this.Numpadd.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Numpadg
+            // 
+            this.Numpadg.Font = new System.Drawing.Font("굴림", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Numpadg.Location = new System.Drawing.Point(234, 198);
+            this.Numpadg.Name = "Numpadg";
+            this.Numpadg.Size = new System.Drawing.Size(65, 65);
+            this.Numpadg.TabIndex = 0;
+            this.Numpadg.Text = "*";
+            this.Numpadg.UseVisualStyleBackColor = true;
+            this.Numpadg.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Numpad0
+            // 
+            this.Numpad0.Font = new System.Drawing.Font("굴림", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Numpad0.Location = new System.Drawing.Point(21, 269);
+            this.Numpad0.Name = "Numpad0";
+            this.Numpad0.Size = new System.Drawing.Size(65, 65);
+            this.Numpad0.TabIndex = 0;
+            this.Numpad0.Text = "0";
+            this.Numpad0.UseVisualStyleBackColor = true;
+            this.Numpad0.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // result
+            // 
+            this.result.Font = new System.Drawing.Font("굴림", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.result.Location = new System.Drawing.Point(163, 269);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(65, 65);
+            this.result.TabIndex = 0;
+            this.result.Text = "=";
+            this.result.UseVisualStyleBackColor = true;
+            this.result.Click += new System.EventHandler(this.result_Click);
+            // 
+            // Clear
+            // 
+            this.Clear.Font = new System.Drawing.Font("굴림", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Clear.Location = new System.Drawing.Point(92, 269);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(65, 65);
+            this.Clear.TabIndex = 0;
+            this.Clear.Text = "C";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.clear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(319, 353);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.Numpad9);
+            this.Controls.Add(this.Numpadg);
+            this.Controls.Add(this.Numpadd);
+            this.Controls.Add(this.Numpad6);
+            this.Controls.Add(this.Numpad3);
+            this.Controls.Add(this.Numpad0);
+            this.Controls.Add(this.Numpad8);
+            this.Controls.Add(this.Numpadm);
+            this.Controls.Add(this.Numpad7);
+            this.Controls.Add(this.Numpad5);
+            this.Controls.Add(this.Numpadp);
+            this.Controls.Add(this.Numpad4);
+            this.Controls.Add(this.Numpad2);
+            this.Controls.Add(this.Numpad1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Numpad1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Numpad2;
+        private System.Windows.Forms.Button Numpad3;
+        private System.Windows.Forms.Button Numpad4;
+        private System.Windows.Forms.Button Numpad5;
+        private System.Windows.Forms.Button Numpad6;
+        private System.Windows.Forms.Button Numpad7;
+        private System.Windows.Forms.Button Numpad8;
+        private System.Windows.Forms.Button Numpad9;
+        private System.Windows.Forms.Button Numpadp;
+        private System.Windows.Forms.Button Numpadm;
+        private System.Windows.Forms.Button Numpadd;
+        private System.Windows.Forms.Button Numpadg;
+        private System.Windows.Forms.Button Numpad0;
+        private System.Windows.Forms.Button result;
+        private System.Windows.Forms.Button Clear;
+    }
+}
+
---
+++ C_Shop_Lesson/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Button btnTmp = (Button)sender;
+            textBox1.Text += btnTmp.Text;
+        }
+
+        private void result_Click(object sender, EventArgs e)
+        {
+            String plus = "+";
+            String minus = "-";
+            String gob = "*";
+            String divide = "/";
+            bool p = textBox1.Text.Contains(plus);
+            bool m = textBox1.Text.Contains(minus);
+            bool g = textBox1.Text.Contains(gob);
+            bool d = textBox1.Text.Contains(divide);
+            String[] numSplit = textBox1.Text.Split('+', '-', '*', '/');
+            double result = Convert.ToDouble(numSplit[0]);
+            if (p == true)
+            {
+                for (int i = 1; i < numSplit.Length; i++)
+                {
+                    result += Convert.ToDouble(numSplit[i]);
+                }
+                textBox1.Text = Convert.ToString(result);
+            }
+            else if (m == true)
+            {
+                for (int i = 1; i < numSplit.Length; i++)
+                {
+                    result -= Convert.ToDouble(numSplit[i]);
+                }
+                textBox1.Text = Convert.ToString(result);
+            }
+            else if (g == true)
+            {
+                for (int i = 1; i < numSplit.Length; i++)
+                {
+                    result *= Convert.ToDouble(numSplit[i]);
+                }
+                textBox1.Text = Convert.ToString(result);
+            }
+            else if (d == true)
+            {
+                for (int i = 1; i < numSplit.Length; i++)
+                {
+                    result /= Convert.ToDouble(numSplit[i]);
+                }
+                textBox1.Text = Convert.ToString(result);
+            }
+        }
+
+        private void clear_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+        }
+    }
+}
