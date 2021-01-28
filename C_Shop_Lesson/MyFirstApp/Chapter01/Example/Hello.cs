8e1a0eb6240ebdbb183e746919d6f0b2bc8145e7
Yorgos7784 <ghkd7784@gmail.com>
2021-01-28 11:05:18 +0900

    20210128 c#

---
+++ C_Shop_Lesson/MyFirstApp/Chapter01/Example/Hello.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter01.Example
+{
+    public class Hello
+    {
+        public void Run()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
---
+++ C_Shop_Lesson/MyFirstApp/Chapter01/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Example.Hello hello = new Example.Hello();
+            //hello.Run();
+
+            Example.Ex001 ex001 = new Example.Ex001();
+            ex001.Run();
+        }
+    }
+}
