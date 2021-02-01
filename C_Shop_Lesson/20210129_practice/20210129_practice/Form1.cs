81b203f962bfadffdcea58b5837dd0a625e51a11
haileykim5418 <rlarkgus5418@naver.com>
2021-02-01 11:08:06 +0900

    20210201 C#

--- C_Shop_Lesson/20210129_practice/20210129_practice/Form1.cs
+++ C_Shop_Lesson/20210129_practice/20210129_practice/Form1.cs
@@ -16,5 +16,78 @@ namespace _20210129_practice
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = button1.Text;
+            textBox1.Text += button1.Text;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = button2.Text;
+            textBox1.Text += button2.Text;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label1.Text = button3.Text;
+            textBox1.Text += button3.Text;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            label1.Text = button4.Text;
+            textBox1.Text += button4.Text;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            label1.Text = button5.Text;
+            textBox1.Text += button5.Text;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            label1.Text = button6.Text;
+            textBox1.Text += button6.Text;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            label1.Text = button7.Text;
+            textBox1.Text += button7.Text;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            label1.Text = button8.Text;
+            textBox1.Text += button8.Text;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            label1.Text = button9.Text;
+            textBox1.Text += button9.Text;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                label2.Text = textBox2.Text;
+                listBox1.Items.Add(textBox2.Text);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listBox1.Items.Add("First Item");
+        }
     }
 }
