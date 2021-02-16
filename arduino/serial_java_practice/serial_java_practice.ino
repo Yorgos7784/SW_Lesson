d51fc3c209678300c394299727a297f55c0758f0
Yorgos7784 <ghkd7784@gmail.com>
2021-02-16 14:06:43 +0900

    20210216 arduino

---
+++ arduino/serial_java_practice/serial_java_practice.ino
@@ -0,0 +1,44 @@
+const int LED = 10;
+const int BUZZER = 8;
+
+#include <Servo.h>
+Servo myservo;
+int pos = 0;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(LED, OUTPUT);
+  pinMode(BUZZER, OUTPUT);
+  myservo.attach(9);
+}
+
+void loop() {
+  controlSystem();
+}
+
+void controlSystem(){
+  if(Serial.available()){
+    char ch = Serial.read();
+    if(ch == '1'){
+      digitalWrite(LED, HIGH);
+    }
+    else if(ch == '2'){
+      digitalWrite(LED, LOW);
+    }
+    else if(ch == '3'){
+      digitalWrite(BUZZER, HIGH);
+      delay(100);
+      digitalWrite(BUZZER, LOW);
+    }
+    else if(ch == '4'){
+      for (pos = 0; pos <= 180; pos += 1) {
+        myservo.write(pos);
+        delay(15);
+      }
+      for (pos = 180; pos >= 0; pos -= 1) {
+        myservo.write(pos);
+        delay(15);
+      }
+    }
+  }
+}
--- java/Java_Work/serial_practice/src/serial_practice/SystemControlProgram.java
+++ java/Java_Work/serial_practice/src/serial_practice/SystemControlProgram.java
@@ -1,8 +1,6 @@
 package serial_practice;
 
-
 import java.util.Scanner;
-
 import jssc.SerialPort;
 import jssc.SerialPortException;
 import jssc.SerialPortList;
@@ -13,40 +11,54 @@ public class SystemControlProgram {
 	static final int BOILER_ON = 3;
 	static final int CLEANER_ON = 4;
 	static final int PROGRAM_EXIT = 5;
-	
-	public static void main(String[] args) {
-		runMenu();
-		
-		
+	static SerialPort serialPort;
+
+	public static int getMenu(Scanner s) {
+		System.out.println("-------------------------");
+		System.out.println("   스마트 홈 프로그램 v1.0");
+		System.out.println("-------------------------");
+		System.out.println("   1. TV 켜기");
+		System.out.println("   2. TV 끄기");
+		System.out.println("   3. 보일러 동작");
+		System.out.println("   4. 세틱기 동작");
+		System.out.println("   5. 프로그램 종료");
+		System.out.println("-------------------------");
+		System.out.print("   메뉴 선택 : ");
+		return s.nextInt();
 	}
 	
-	public static void runMenu() {
+	public static void getSerialPort() {
+		String[] portNames = SerialPortList.getPortNames();
+		System.out.println(portNames[0]);
+		serialPort = new SerialPort(portNames[0]);
+		try {
+			serialPort.openPort();
+			serialPort.setParams(SerialPort.BAUDRATE_9600, SerialPort.DATABITS_8, SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);
+		} catch (SerialPortException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) throws SerialPortException {
+		getSerialPort();
 		Scanner s = new Scanner(System.in);
-		
 		while(true) {
-			System.out.println("-------------------------");
-			System.out.println("   스마트 홈 프로그램 v1.0");
-			System.out.println("-------------------------");
-			System.out.println("   1. TV 켜기");
-			System.out.println("   2. TV 끄기");
-			System.out.println("   3. 보일러 동작");
-			System.out.println("   4. 세틱기 동작");
-			System.out.println("   5. 프로그램 종료");
-			System.out.println("-------------------------");
-			System.out.print("   메뉴 선택 : ");
-			int menu = s.nextInt();
-			switch(menu) {
+			switch(getMenu(s)) {
 			case TV_ON :
 				System.out.println("TV ON!");
+				serialPort.writeInt(Character.forDigit(TV_ON, 10));
 				break;
 			case TV_OFF :
 				System.out.println("TV OFF!");
+				serialPort.writeInt(Character.forDigit(TV_OFF, 10));
 				break;
 			case BOILER_ON :
 				System.out.println("BOILER ON!");
+				serialPort.writeInt(Character.forDigit(BOILER_ON, 10));
 				break;
 			case CLEANER_ON :
 				System.out.println("CLEANER ON!");
+				serialPort.writeInt(Character.forDigit(CLEANER_ON, 10));
 				break;
 			case PROGRAM_EXIT :
 				System.out.println("BYE!");
@@ -57,3 +69,22 @@ public class SystemControlProgram {
 		}
 	}
 }
+
+//class WriteThread extends Thread{
+//	SerialPort serial;
+//
+//	WriteThread(SerialPort serial) {
+//		this.serial = serial;
+//	}
+//
+//	public void run() {
+//		try {
+//			int c = 0;
+//			while ((c = System.in.read()) > -1) {
+//				serial.writeInt(c);
+//			}
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//	}
+//}
