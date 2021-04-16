e3c9a53ee8a42d5623ce434adc32c0f472d915f7
Yorgos7784 <ghkd7784@gmail.com>
2021-04-16 16:38:17 +0900

    20210416 Portfolio

---
+++ Portfolio/ParkingManagementProgram/ParkingManagementProgram/DataManager.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Linq;
+
+namespace ParkingManagementProgram
+{
+    class DataManager
+    {
+        public static List<ParkingCar> Cars = new List<ParkingCar>();
+        static DataManager()
+        {
+            Load();
+        }
+
+        public static void Load()
+        {
+            Cars.Clear();
+            try
+            {
+                string carsOutput = File.ReadAllText(@"./Cars.xml");
+                XElement carXElement = XElement.Parse(carsOutput);
+                foreach (var item in carXElement.Descendants("car"))
+                {
+                    int tempParkingSpot = int.Parse(item.Element("parkingSpot").Value);
+                    string tempCarNumber = item.Element("carNumber").Value;
+                    string tempDriverName = item.Element("driverName").Value;
+                    string tempPhoneNumber = item.Element("phoneNumber").Value;
+                    DateTime tempParkingTime = item.Element("parkingTime").Value == "" ?
+                        DateTime.Now : DateTime.Parse(item.Element("parkingTime").Value);
+
+                    ParkingCar tempCar = new ParkingCar()
+                    {
+                        parkingSpot = tempParkingSpot,
+                        carNumber = tempCarNumber,
+                        driverName = tempDriverName,
+                        phoneNumber = tempPhoneNumber,
+                        parkingTime = tempParkingTime
+                    };
+                    Cars.Add(tempCar);
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.Message);
+                printLog(ex.Message);
+                printLog(ex.StackTrace);
+
+                // 만약 파일이 없어서 진입한 경우
+                CreateFile(); // 파일 생성
+                Save(); // 파일저장
+                Load(); // 다시 불러오기
+                // Load함수에 문제가 있을 시 무한루프에 빠진다.
+            }
+        }
+
+        private static void CreateFile()
+        {
+            string fileName = @"./Cars.xml";
+            StreamWriter writer = File.CreateText(fileName);// 파일이 없으면 해당 파일 생성
+            writer.Dispose(); // 메모리 해제
+        }
+
+        public static void Save()
+        {
+            string booksOutput = string.Empty;
+            booksOutput += "<cars>\n";
+            if (Cars.Count > 0)
+            {
+                foreach (var item in Cars)
+                {
+                    booksOutput += "<car>\n";
+                    booksOutput += $"   <parkingSpot>{item.parkingSpot}</parkingSpot>\n";
+                    booksOutput += $"   <carNumber>{item.carNumber}</carNumber>\n";
+                    booksOutput += $"   <driverName>{item.driverName}</driverName>\n";
+                    booksOutput += $"   <phoneNumber>{item.phoneNumber}</phoneNumber>\n";
+                    booksOutput += $"   <parkingTime>{item.parkingTime}</parkingTime>\n";
+                    booksOutput += "</car>\n";
+                }
+                
+            }
+            else //xml파일에 아무것도 없는 경우
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    booksOutput += "<car>\n";
+                    booksOutput += $"   <parkingSpot>{i}</parkingSpot>\n";
+                    booksOutput += "   <carNumber></carNumber>\n";
+                    booksOutput += "   <driverName></driverName>\n";
+                    booksOutput += "   <phoneNumber></phoneNumber>\n";
+                    booksOutput += "   <parkingTime></parkingTime>\n";
+                    booksOutput += "</car>\n";
+                }
+            }
+            booksOutput += "</cars>";
+            File.WriteAllText(@"./Cars.xml", booksOutput);
+        }
+
+        // 두번째 파라미터를 적지 않으면 name에는 parkingHistory가 들어감
+        public static void printLog(string contents, string name = "parkingHistory")
+        {
+            string directory = "parkingHistory";
+            DirectoryInfo di = new DirectoryInfo(directory);
+            if (!di.Exists)
+            {
+                di.Create(); // 폴더만들기
+            }
+
+            // @ : \ 하나 쌉가능 (없으면 \\)
+            //using (StreamWriter writer = new StreamWriter(@"parkingHistory\parkingHistory.txt", true))
+            using (StreamWriter writer = new StreamWriter(directory + @"\" + name + ".txt", true))
+            {
+                writer.WriteLine(contents);
+            }
+        }
+
+
+    }
+}
---
+++ Portfolio/ParkingManagementProgram/ParkingManagementProgram/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ParkingManagementProgram
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //DataManager.Load();
+            /*List<ParkingCar> cars = new List<ParkingCar>();
+            cars.Add(new ParkingCar() 
+            { 
+                parkingSpot = 1, 
+                carNumber = "30고9468", 
+                driverName = "황창식", 
+                phoneNumber = "010-6626-7784", 
+                parkingTime = DateTime.Now 
+            });*/
+
+            dataGridView1.DataSource = DataManager.Cars;
+            textBox1.Text = DataManager.Cars[0].parkingSpot.ToString();
+            textBox2.Text = DataManager.Cars[0].carNumber;
+            textBox3.Text = DataManager.Cars[0].driverName;
+            textBox4.Text = DataManager.Cars[0].phoneNumber;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text = "지금은 : " + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + " 입니다.";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            writeLog("주차버튼 클릭", DateTime.Now.ToString("yyyy_MM_dd"));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            writeLog("출차버튼 클릭", DateTime.Now.ToString("yyyy_MM_dd"));
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            writeLog("조회버튼 클릭", DateTime.Now.ToString("yyyy_MM_dd"));
+        }
+
+        private void writeLog(string contents)
+        {
+            string logContents = $"[{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")}]{contents}";
+
+            // 옛것이 좋은것이여~(새로운 내용 밑으로 추가)
+            //listBox1.Items.Add(logContents);
+
+            // 신문물이 짱이다(새로운 내용 위로 추가)
+            listBox1.Items.Insert(0, logContents);
+            DataManager.printLog(logContents);
+
+        }
+
+        private void writeLog(string contents, string date)
+        {
+            string logContents = $"[{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")}]{contents}";
+            listBox1.Items.Insert(0, logContents);
+            DataManager.printLog(logContents, date);
+        }
+    }
+}
